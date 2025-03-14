c202ac7d2cff0f8f48805bce30ea2c1aff4f07a4
HaydukRoss <haydukross@gmail.com>
2025-03-14 03:22:02 -0700

    Lab4 Again

---
+++ IST526/Lab4/story_content/6BVWZzitkr5_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.93.33359.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:04.476%0D%0AThen%20Browse%20the%20various%20Voices%20in%20the%20ElevenLabs%20library%20by%20Clicking%20%E2%80%9CVoice%E2%80%9D%0D%0A%0D%0A"}')
