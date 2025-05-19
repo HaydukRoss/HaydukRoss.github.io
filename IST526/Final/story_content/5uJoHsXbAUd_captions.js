e1229c3b7154f33456656619bc1fff413d5f483a
HaydukRoss <haydukross@gmail.com>
2025-05-18 21:46:40 -0700

    Final Folder

---
+++ IST526/Final/story_content/5uJoHsXbAUd_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.93.33359.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:05.451%0D%0ASelect%20a%20photo%20from%20your%20folder.%20%20For%20this%20exercise,%20click%20on%20%E2%80%9CPixar%20ChatGPT%E2%80%9D.%0D%0A%0D%0A"}')
