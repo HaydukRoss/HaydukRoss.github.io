4afea10a512eba6cca28e91e3df50da20c93335e
HaydukRoss <haydukross@gmail.com>
2025-10-28 19:41:15 -0700

    699

---
+++ IST699/Storyline/story_content/6a9mhsYpd8Q_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.93.33359.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:03.594%0D%0AOnce%20the%20video%20has%20processed,%20click%20on%20the%20video%20to%20review.%0D%0A%0D%0A"}')
