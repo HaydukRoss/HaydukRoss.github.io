51b66b5d5bfcee968166418c83fc6cc0a3dc8313
HaydukRoss <haydukross@gmail.com>
2026-04-25 11:36:23 -0700

    AHF LIVE SAT 2

---
+++ AHFLIVE/story_content/6MvR2fEBgLo_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":1080,"text":"Hello and welcome to AHF University."},{"start":4500,"text":"My name is Ross and I just want to say"},{"start":7400,"text":"we are so happy you are a volunteer with AHF"},{"start":11239,"text":"and we are truly grateful for your support."},{"start":14239,"text":"What you give to out of the closest makes this mission possible."},{"start":18280,"text":"This training is our way of making sure you feel as prepared"},{"start":21790,"text":"and as valued as you are."},{"start":24079,"text":"Thank you for being here."}]}]};
+    window.globalLoadJsAsset('story_content/6MvR2fEBgLo_transcripts.js', JSON.stringify(data));
+})();
